777b0e4dd1b0e52b7096f9de05ceedc675c6fd6d
Chris Nusbaum <pixelrex@gmail.com>
2011-11-16 22:14:33 -0700

    images

--- js/script.js
+++ js/script.js
@@ -2,17 +2,44 @@
 
 */
 
-$("document").ready(function() { 
+var thisImage =  1,
+	i = 1,
+	z = 1,
+	thisPath = "img/slides/",
+	totalQuads = $("body div").length,
+	totalImages = 3;
 
-	function changeSlide() {
-		var thisImage =  ,
-			thisPath = "img/gifs/";
-		$("html").css("background", "url(" thisPath + thisImage + ") no-repeat");
-	}
+console.log(totalQuads);
 
-	setTimeout( changeSlide, 1000);
+$(document).ready(function() {
+
+	function slideChange() {
+		
+		if (i > totalQuads) {
+			i = 1;
+		}
+		
+		if (thisImage > totalImage) { 
+			thisImage = 1;
+		}
+/*
+		for(var x = 0; x < totalQuads; x++) {
+		}*/
+
+		
+		$("#quad" + i).css("background", " #000 url(" + thisPath + thisImage +".gif) no-repeat center center");
+		console.log(i);
+
+
+		i++;
+		z++;
+	}//slideChange
+
+    setInterval(slideChange, 1000);
 
 });
+
+
 
 
 
